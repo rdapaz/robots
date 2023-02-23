6d799eccf81f24bf50f742806c5a34d5e262eb15
Ricardo da Paz <ric@dapaz.net>
2023-02-23 20:26:24 +0800

    Major mods

--- controller/joystick_modbus.py
+++ controller/joystick_modbus.py
@@ -1,10 +1,27 @@
+from paho.mqtt import client as mqtt_client
+import random
 import pygame
-import pymodbus
-from pymodbus.client import ModbusTcpClient
 
 # Initialize pygame and the joystick
 pygame.init()
 pygame.joystick.init()
+
+broker = '10.11.12.77'
+port = 1883
+client_id = f'python-mqtt-{random.randint(0,1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker")
+        else:
+            print("Failed to connect, return code {rc}")
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
 
 joystick_count = pygame.joystick.get_count()
 print(f'[+] {joystick_count} joystick(s) found!')
@@ -12,8 +29,8 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 # Create a Modbus TCP client
-client = ModbusTcpClient('localhost')
-client.connect()
+client = connect_mqtt()
+
 while True:
     # Poll the joystick for events
     for event in pygame.event.get():
@@ -22,33 +39,32 @@ while True:
             print(event.value)
             # Handle joyhatmotion UP event
             if event.value[1] == 1:
-                client.write_registers(0x1, 2)
+                client.publish('joystick/command', 2)
                 print('[+] UP Event Handled')
             # Handle joyhatmotion DOWN event
             if event.value[1] == -1:
-                client.write_registers(0x1, 8)
+                client.publish('joystick/command', 8)
                 print('[+] DOWN Event Handled')
             # Handle joyhatmotion LEFT event
             if event.value[0] == -1:
-                client.write_registers(0x1, 4)
+                client.publish('joystick/command', 4)
                 print('[+] LEFT Event Handled')
             # Handle joyhatmotion RIGHT event
             if event.value[0] == 1:
-                client.write_registers(0x1, 6)
+                client.publish('joystick/command', 6)
                 print('[+] RIGHT Event Handled')
             # Handle joyhatmotion NEUTRAL POSITION event
             if event.value == (0,0):
-                client.write_registers(0x1, 0)
+                client.publish('joystick/command', 0)
         elif event.type == pygame.JOYBUTTONDOWN:
             # Handle joyhatmotion RIGHT TRIGGER event
             if event.button == 5:
-                client.write_registers(0x1, 6842)
+                client.publish('joystick/command', 6842)
                 print('[+] RIGHT TRIGGER Event Handled')
             # Handle joyhatmotion LEFT TRIGGER event
             if event.button == 4:
-                client.write_registers(0x1, 6248)
+                client.publish('joystick/command', 6248)
                 print('[+] LEFT TRIGGER Event Handled')
         elif event.type == pygame.JOYBUTTONUP:
-            client.write_registers(0x1, 0)
-# Close the Modbus TCP client when the program is finished
-client.close()
+            client.publish('joystick/command', 0)
+
--- device/main.py
+++ device/main.py
@@ -2,6 +2,7 @@ from PiicoDev_VL53L1X import PiicoDev_VL53L1X
 from PiicoDev_LIS3DH import PiicoDev_LIS3DH
 from PiicoDev_SSD1306 import *
 from PiicoDev_Unified import sleep_ms
+from mqttsimple import MQTTClient
 
 import machine
 import utime
@@ -18,12 +19,12 @@ def connect():
     wlan.connect(ssid, password)
     while wlan.isconnected() == False:
         print('Waiting for connection...')
-        sleep(1)
-    print(wlan.ifconfig())
+        sleep_ms(1000)
+    return wlan.ifconfig()
 
 
 class MotorDriver():
-    def __init__(self,M1A,M1B,M2A,M2B):
+    def __init__(self,M1A,M2A,M1B,M2B):
         
         self.M1A = machine.PWM(machine.Pin(M1A))
         self.M1B = machine.PWM(machine.Pin(M1B))
@@ -54,72 +55,144 @@ class MotorDriver():
     def brake(self):
         self.speed(0,0)
 
-Front = MotorDriver(1,2,4,5)
-Back = MotorDriver(11,12,14,15)
 
-def StraightAhead(s):
-    Front.speed(s,s)
-    Back.speed(s,s)
+class MotorControl():
+    def __init__(self, Front_M1A,Front_M1B,Front_M2A,Front_M2B,\
+                 Rear_M1A,Rear_M1B,Rear_M2A,Rear_M2B):
+        self.Front = MotorDriver(Front_M1A,Front_M1B,Front_M2A,Front_M2B)
+        self.Back = MotorDriver(Rear_M1A,Rear_M1B,Rear_M2A,Rear_M2B)
+        self.speed = 100
+
+    def StraightAhead(self):
+        self.Front.speed(self.speed,self.speed)
+        self.Back.speed(self.speed,self.speed)
     
-def StraightReverse(s):
-    Front.speed(-s,-s)
-    Back.speed(-s,-s)
+    def StraightReverse(self):
+        self.Front.speed(-self.speed,-self.speed)
+        self.Back.speed(-self.speed,-self.speed)
 
-def StraightLeft(s):
-    Front.speed(-s,s)
-    Back.speed(s,-s)
+    def StraightLeft(self):
+        self.Front.speed(-self.speed,self.speed)
+        self.Back.speed(self.speed,-self.speed)
 
-def StraightRight(s):
-    Front.speed(s,-s)
-    Back.speed(-s,s)
+    def StraightRight(self):
+        self.Front.speed(self.speed,-self.speed)
+        self.Back.speed(-self.speed,self.speed)
 
-def DiagFwdLeft(s):
-    Front.speed(0,s)
-    Back.speed(s,0)
+    def DiagFwdLeft(self):
+        self.Front.speed(0,self.speed)
+        self.Back.speed(self.speed,0)
 
-def DiagFwdRight(s):
-    Front.speed(s,0)
-    Back.speed(0,s)
+    def DiagFwdRight(self):
+        self.Front.speed(self.speed,0)
+        self.Back.speed(0,self.speed)
 
-def DiagRevLeft(s):
-    Front.speed(-s,0)
-    Back.speed(0,-s)
+    def DiagRevLeft(self):
+        self.Front.speed(-self.speed,0)
+        self.Back.speed(0,-self.speed)
 
-def DiagRevRight(s):
-    Front.speed(0,-s)
-    Back.speed(-s, 0)
+    def DiagRevRight(self):
+        self.Front.speed(0,-self.speed)
+        self.Back.speed(-self.speed, 0)
+        
+    def Halt(self):
+        self.Front.brake()
+        self.Back.brake()
 
 
+joystick_command = ''
 distSensor = PiicoDev_VL53L1X()
 motion = PiicoDev_LIS3DH() # Initialise the accelerometer
 display = create_PiicoDev_SSD1306()
 
 motion.range = 2 # Set the range to +-2g
+connected = False
+
+
+# Set up MQTT client
+client = MQTTClient(client_id="pico", server="10.11.12.77")
+
+# Define callback function for incoming messages
+def message_callback(topic, msg):
+    global joystick_command
+    joystick_command = msg
+
+# Define a delay in microseconds (here, we use a 5-second delay)
+delay_us = 5000000
+delay_2s = 2000000
+
+
+# Get the current time in microseconds
+start_time = utime.ticks_us()
+ips = connect()
+
+# Wait for the specified delay
+while utime.ticks_diff(utime.ticks_us(), start_time) < delay_us:
+    pass
+
+# Connect to MQTT broker and subscribe to desired topics
+client.connect()
+client.set_callback(message_callback)
+client.subscribe('joystick/command')
+
+# Listen for incoming messages and display topic in console
 
 while True:
-    try:
-        connect()
-    except KeyboardInterrupt:
-        machine.reset()
-    print('[+] Successfully connected!')
+    # ips = connect()
     dist = distSensor.read() 
-    st_out1 = f"{dist} mm"
     print(str(dist) + " mm")
     
+    client.check_msg()
     x, y, z = motion.acceleration
     x = round(x,2) 
     y = round(y,2)
     z = round(z,2)
-    
+
+    controller = MotorControl(4,5,9,10,14,15,19,20)
+         
+    st_out1 = f"{dist} mm"    
     st_out2 = f"x={x}"
     st_out3 = f"y={y}"
     st_out4 = f"z={z}"
+    
     display.fill(0)
     display.text(st_out1,30,5,1)
     display.text(st_out2,30,15,1) 
     display.text(st_out3,30,25,1) 
-    display.text(st_out4,30,35,1) 
+    display.text(st_out4,30,35,1)
+    # display.text(ips[0],30,45,1)
+    display.text(joystick_command,30,45,1)
+    action_display = lambda x: display.text(x,30,45,1)
+    print(joystick_command)
+    if dist >= 40:
+        if joystick_command == b'2':
+            action_display('Forward')
+            controller.StraightAhead()
+        elif joystick_command == b'8':
+            action_display('Backward')
+            controller.StraightReverse()
+        elif joystick_command == b'4':
+            action_display('Left')
+            controller.StraightLeft()
+        elif(joystick_command) == b'6':
+             action_display('Right')
+             controller.StraightRight()
+        elif(joystick_command) == b'6248':
+            action_display('DiagFwdLft')
+            controller.DiagFwdLeft()
+        elif(joystick_command) == b'6842':
+            action_display('DiagFwdRgt')
+            controller.DiagFwdRight()
+        elif(joystick_command) == b'0':
+            action_display('Neutral')
+            controller.Halt()
+    else:
+        controller.Halt()
+        action_display('Panic!')
+        stt1 = utime.tick_us()
+        while utime.tick_us() - stt1 < delay_2s:
+            controller.StraightReverse()
+        
     display.show()
     sleep_ms(500)
-
 
